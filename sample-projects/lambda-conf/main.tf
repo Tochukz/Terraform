a96b6f67348c00bc31905e0875f42986ff7ecae4
Tochukwu Nwachukwu <truetochukz@gmail.com>
2023-04-20 02:43:35 +0200

    Lambda resource

---
+++ sample-projects/lambda-conf/main.tf
@@ -0,0 +1,184 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "app_version" {
+  description = "The version of the application build"
+  default = "1.0.0"
+}
+
+resource "aws_vpc" "lambda_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true 
+  enable_dns_support = true
+
+  tags = {
+    Name = "LamdaConfVpc"
+  }
+}
+
+resource "aws_subnet" "lambda_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.lambda_vpc.id 
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "LambdaConfSubnet"
+  }
+}
+
+resource "aws_security_group" "lambda_security_group" {
+  name_prefix = "lambda-conf"
+  vpc_id = aws_vpc.lambda_vpc.id 
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = [ "10.0.1.0/24" ] 
+  }
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal =  {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "lambda-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement: [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = ["arn:aws:logs:*:*:*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  policy_arn = aws_iam_policy.lambda_policy.arn
+  role = aws_iam_role.lambda_role.name
+}
+
+
+resource "aws_lambda_function" "lambda_func" {
+  function_name = "lambda-func-app" 
+
+#   filename =  "main.zip"
+#   source_code_hash = filebase64sha256("main.zip")
+  s3_bucket = "tochukwu1-bucket"
+  s3_key = "v${var.app_version}/main.zip"
+
+  handler = "main.handler"
+  role = aws_iam_role.lambda_role.arn
+  runtime = "nodejs18.x"
+  vpc_config {
+    subnet_ids = [aws_subnet.lambda_subnet.id]
+    security_group_ids = [aws_security_group.lambda_security_group.id]
+  }
+}
+
+# resource "aws_api_gateway_rest_api" "lambda_rest_api" {
+#   name = "LambdaRestAPI" 
+#   description = "Lambda Rest API Configuration"
+# }
+
+# resource "aws_api_gateway_resource" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id 
+#   parent_id = aws_api_gateway_rest_api.lambda_rest_api.root_resource_id
+#   path_part = "{proxy+}"
+# }
+
+# resource "aws_api_gateway_method" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
+#   resource_id = aws_api_gateway_resource.proxy.id
+#   http_method = "ANY"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "lambda" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
+#   resource_id = aws_api_gateway_method.proxy.resource_id
+#   http_method = aws_api_gateway_method.proxy.http_method
+
+#   integration_http_method = "POST"
+#   type = "AWS_PROXY"
+#   uri = aws_lambda_function.lambda_func.invoke_arn
+# }
+
+# resource "aws_api_gateway_method" "proxy_root" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
+#   resource_id = aws_api_gateway_rest_api.lambda_rest_api.root_resource_id
+#   http_method = "ANY"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "lambda_root" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id 
+#   resource_id = aws_api_gateway_method.proxy_root.resource_id
+#   http_method = aws_api_gateway_method.proxy.http_method
+
+#   integration_http_method = "POST"
+#   type = "AWS_PROXY"
+#   url = aws_lambda_function.lambda_func.invoke_arn
+# }
+
+# resource "aws_api_gateway_deployment" "simple" {
+#   depends_on = [
+#     aws_api_gateway_integration.lambda,
+#     aws_api_gateway_integration.lambda_root
+#   ]
+#   rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id 
+#   stage_name = "develop"
+# }
+
+# resource "aws_lambda_permission" "apigw" {
+#   statement_id = "AllowAPIGatewayInvoke"
+#   action = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.lambda_func.function_name
+#   principal = "apigateway.amazonaws.com"
+#   source_arn = aws_api_gateway_rest_api.lambda_rest_api.execution_arn
+# }
+
+# output "base_url" {
+#   value = aws_api_gateway_deployment.simple.invoke_url
+# }
---
+++ sample-projects/lambda-conf/readme.md
@@ -0,0 +1,38 @@
+__Zip the source code build__  
+```
+$ cd example-app
+$ zip  ../main.zip main.js  
+```  
+In a real build and deploy scenario we would have an S3 bucket set aside for staging our archive and would use this to "hand off" these artifacts between the build and deploy process.
+
+__Copy the build to an S3 bucket__  
+```
+$ aws s3 cp main.zip s3://tochukwu1-bucket/v1.0.0/main.zip
+```  
+The version number is included in the object path.   
+
+__Trigger the Lambda function using AWS CLI__  
+```
+$ aws lambda invoke --function-name lambda-func-app output.json 
+```  
+
+__To deploy a new version of the application__  
+1. Update the body string in example-app/main.js file 
+2. create a new zip and upload it with a new version
+```
+$ cd example-app
+$ zip  ../main.zip main.js  
+$ aws s3 cp main.zip s3://tochukwu1-bucket/v1.0.1/main.zip
+```
+3. Deploy the new build 
+Deploy the new version using the app_version variable 
+```
+$ terraform apply -var="app_version=1.0.1"
+``` 
+Alternatively you can use a variable file 
+````
+$ terraform apply --var-file lambda.tfvars
+```
+
+__Learn more__  
+[Serverless Applications with AWS Lambda and API Gateway](https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs/guides/serverless-with-aws-lambda-and-api-gateway)  
