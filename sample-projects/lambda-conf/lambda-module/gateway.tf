50700e603fa71c7224cb118c15e3072a4579bc09
Tochukwu Nwachukwu <truetochukz@gmail.com>
2023-04-22 15:39:52 +0200

    Lambda module

--- sample-projects/lambda-conf/lambda-module/gateway.tf
+++ sample-projects/lambda-conf/lambda-module/gateway.tf
@@ -1,23 +1,23 @@
-resource "aws_api_gateway_rest_api" "lambda_rest_api" {
+resource "aws_api_gateway_rest_api" "rest" {
   name        = "${var.app_name}LambdaRestAPI"
   description = "Lambda Rest API Configuration"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
-  parent_id   = aws_api_gateway_rest_api.lambda_rest_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.lambda_rest_api.id
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
+  rest_api_id = aws_api_gateway_rest_api.rest.id
   resource_id = aws_api_gateway_method.proxy.resource_id
   http_method = aws_api_gateway_method.proxy.http_method
 
@@ -27,16 +27,16 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.lambda_rest_api.id
-  resource_id   = aws_api_gateway_rest_api.lambda_rest_api.root_resource_id
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_rest_api.rest.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
+  rest_api_id = aws_api_gateway_rest_api.rest.id
   resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -48,8 +48,13 @@ resource "aws_api_gateway_deployment" "simple" {
     aws_api_gateway_integration.lambda,
     aws_api_gateway_integration.lambda_root
   ]
-  rest_api_id = aws_api_gateway_rest_api.lambda_rest_api.id
-  stage_name  = "develop"
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+}
+
+resource "aws_api_gateway_stage" "simple" {
+  deployment_id = aws_api_gateway_deployment.simple.id
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  stage_name    = "develop"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -57,7 +62,7 @@ resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.simple.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.lambda_rest_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.rest.execution_arn}/*/*"
 }
 
 
