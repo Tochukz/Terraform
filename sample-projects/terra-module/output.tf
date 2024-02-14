31b031d484f668f2555e05a496844159e9116fdc
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-02-14 21:59:24 +0200

    Using terraform module

---
+++ sample-projects/terra-module/output.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_bucket_domain_name" {
+  value = module.s3_bucket.s3_bucket_bucket_domain_name
+}
+output "s3_bucket_arn" {
+  value = module.s3_bucket.s3_bucket_arn
+}
+output "s3_bucket_bucket_regional_domain_name" {
+  value = module.s3_bucket.s3_bucket_bucket_regional_domain_name
+}
