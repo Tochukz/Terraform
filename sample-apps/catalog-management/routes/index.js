aa4d4750ab53d1d6146d1255bc8260ad3f69ce61
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-10-13 19:13:04 +0200

    Catalog Management

---
+++ sample-apps/catalog-management/routes/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+
+const categories = [
+  {
+    categoryId: 1,
+    name: "Physical Science",
+  },
+  {
+    categoryId: 2,
+    name: "Live Science",
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Catalog Management Server" });
+});
+
+router.get("/categories", function (req, res, next) {
+  return res.json(categories);
+});
+
+router.post("/categories/create", (req, res, next) => {
+  const categoryName = req.body.categoryName;
+  const categoryId = categories.length + 1;
+  const newCategory = { categoryId, categoryName };
+  categories.push(newCategory);
+  return res.status(201).json(newCategory);
+});
+
+module.exports = router;
