e21badb27ba95fe8db075b8dbd4e348ff93f4e29
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-10-18 11:46:45 +0200

    User Management

--- sample-apps/user-management/routes/users.js
+++ sample-apps/user-management/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const users = [
+  {
+    name: "James Young",
+  },
+];
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/list", function (req, res, next) {
+  res.json(users);
 });
 
 module.exports = router;
